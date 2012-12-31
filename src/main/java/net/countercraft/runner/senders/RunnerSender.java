e3cf7d11dc1588021f76bf725ee626af6bac7d32
Koen Vlaswinkel <koen@vlaswinkel.info>
2012-12-31 09:48:50 +0100

    Removed override annotations in RunnerSender

--- src/main/java/net/countercraft/runner/senders/RunnerSender.java
+++ src/main/java/net/countercraft/runner/senders/RunnerSender.java
@@ -26,51 +26,44 @@ public class RunnerSender extends ConsoleCommandSender {
 		this.nickname = nickname;
 		this.use_nick = use_nick;
 	}
-	@Override
+	
 	public void sendMessage(String arg0) {
 		Controller.getXMPPManager().sendToOne(ChatColor.stripColor(arg0), participant);
 	}
 
-	@Override
 	public void sendMessage(String[] arg0) {
 		for (String msg : arg0) {
 			Controller.getXMPPManager().sendToOne(ChatColor.stripColor(msg), participant);
 		}
 	}
 
-	@Override
 	public PermissionAttachment addAttachment(Plugin arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public PermissionAttachment addAttachment(Plugin arg0, int arg1) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public PermissionAttachment addAttachment(Plugin arg0, String arg1,
 			boolean arg2) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public PermissionAttachment addAttachment(Plugin arg0, String arg1,
 			boolean arg2, int arg3) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Set<PermissionAttachmentInfo> getEffectivePermissions() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public boolean hasPermission(String arg0) {
 		if (this.use_nick && Controller.isVaultInstalled()) {
 			return Controller.getPermission().playerHas((World)null, this.nickname.toString(), arg0);
@@ -78,7 +71,6 @@ public class RunnerSender extends ConsoleCommandSender {
 		return true;
 	}
 
-	@Override
 	public boolean hasPermission(Permission arg0) {
 		if (this.use_nick && Controller.isVaultInstalled()) {
 			return Controller.getPermission().playerHas((World)null, this.nickname.toString(), arg0.getName());
@@ -86,44 +78,36 @@ public class RunnerSender extends ConsoleCommandSender {
 		return true;
 	}
 
-	@Override
 	public boolean isPermissionSet(String arg0) {
 		return true;
 	}
 
-	@Override
 	public boolean isPermissionSet(Permission arg0) {
 		return true;
 	}
 
-	@Override
 	public void recalculatePermissions() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void removeAttachment(PermissionAttachment arg0) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public boolean isOp() {
 		return true;
 	}
 
-	@Override
 	public void setOp(boolean arg0) {
 		
 	}
 
-	@Override
 	public String getName() {
 		return this.participant;
 	}
 
-	@Override
 	public Server getServer() {
 		return Bukkit.getServer();
 	}
